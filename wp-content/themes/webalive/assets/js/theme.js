bc1e1d1674f9fabf762976fdbd49faba2bb391e0
rasadin <12105708+rasadin@users.noreply.github.com>
2024-06-22 14:56:26 +0600

    test

--- wp-content/themes/webalive/assets/js/theme.js
+++ wp-content/themes/webalive/assets/js/theme.js
@@ -15,9 +15,21 @@
     
 
     $(document).ready(function() {
-        $('input[type="radio"]').on('click', function() {
+        $('input[name="service"]').on('click', function() {
             // Remove active class from all radio button labels
-            $('input[type="radio"]').parent().removeClass('active');
+            $('input[name="service"]').parent().removeClass('active');
+    
+            // Add active class to the clicked radio button's label
+            if ($(this).is(':checked')) {
+                $(this).parent().addClass('active');
+            }
+        });
+    });
+
+    $(document).ready(function() {
+        $('input[name="cost"]').on('click', function() {
+            // Remove active class from all radio button labels
+            $('input[name="cost"]').parent().removeClass('active');
     
             // Add active class to the clicked radio button's label
             if ($(this).is(':checked')) {
