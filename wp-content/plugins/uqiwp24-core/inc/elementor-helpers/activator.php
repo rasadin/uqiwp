e4369ebfe8cdc0525da3584429de068e1400ca78
rasadin <12105708+rasadin@users.noreply.github.com>
2024-07-13 23:57:06 +0600

    updated

--- wp-content/plugins/uqiwp24-core/inc/elementor-helpers/activator.php
+++ wp-content/plugins/uqiwp24-core/inc/elementor-helpers/activator.php
@@ -3,7 +3,7 @@
 function uqiwp24_elements_activator() {
 	require_once WAE_PLUGIN_PATH . '/elements/client-slider.php';
 	require_once WAE_PLUGIN_PATH . '/elements/service-list.php';
-	require_once WAE_PLUGIN_PATH . '/elements/right-to-left.php';
+	require_once WAE_PLUGIN_PATH . '/elements/owl-right-to-left.php';
 }
 add_action('elementor/widgets/widgets_registered','uqiwp24_elements_activator');
 
